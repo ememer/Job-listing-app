5c8a213b78d7f0cf924d7c783a257db84d5f803a
Sopel <Michal.Sopel@Honeywell.com>
2022-11-29 09:41:12 +0100

    Displaying tools and languages filters

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,34 +6,52 @@ import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import "./Search.css";
 import clsx from "clsx";
 
+import data from "./../utils/data.json";
+
 const SearchComponent = () => {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const getUniqueElements = (data, key) => {
+    let arrayOfEachElements = [];
+    data.map((elements) =>
+      elements?.[key].forEach((element) => arrayOfEachElements.push(element))
+    );
+
+    return [...new Set(arrayOfEachElements)];
+  };
+
+  const onClose = (e) => {
+    if (e.target.id === "search-section") {
+      setIsSearchOpen(false);
+    }
+  };
+
   return (
     <div
       id="search-section"
       className={clsx(isSearchOpen && " enabled", "search-section")}
-      onClick={(e) => {
-       
-        if (e.target.id === "search-section") {
-          setIsSearchOpen(false);
-        }
-      }}
+      onClick={(e) => onClose(e)}
     >
       <div
         onClick={(e) => {
-          console.log("T", e.target.id);
           if (
             e.target.id !== "remove_button" &&
-            e.target.id !== "clear_button"
+            e.target.id === "search_content"
           ) {
             setIsSearchOpen(true);
           }
         }}
         className="search-component"
+        id="search_content"
       >
-        <div className="search-items">
-          <ul>
-            <li id="remove_button">
+        <div
+          className={clsx(
+            isSearchOpen ? "enabled-search-items" : "disabled-search-items",
+            "search-items"
+          )}
+        >
+          <ul id="search_content">
+            <li>
               <span>TEST</span>
               <button>
                 <FontAwesomeIcon icon={faXmark} />
@@ -41,8 +59,31 @@ const SearchComponent = () => {
             </li>
           </ul>
           <button id="clear_button">Wyczyść</button>
+          {isSearchOpen && (
+            <button onClick={() => setIsSearchOpen(false)}>
+              <FontAwesomeIcon icon={faXmark} />
+            </button>
+          )}
         </div>
       </div>
+      {isSearchOpen && (
+        <div className="search-filters">
+          <div>
+            <ul>
+              {getUniqueElements(data, "tools").map((tool) => (
+                <li key={tool}>{tool}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <ul>
+              {getUniqueElements(data, "languages").map((language) => (
+                <li key={language}>{language}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
