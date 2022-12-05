9a884dc60bef6ca0df5c726541f75ca44982484a
ememer <46853050+ememer@users.noreply.github.com>
2022-12-05 19:13:24 +0100

    Set types for context

--- src/Context/JobsListProvider.tsx
+++ src/Context/JobsListProvider.tsx
@@ -1,24 +1,27 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import { JobListContext } from "./JobsListContext";
-import jobLists from "./../utils/data.json";
+import { JobListContext } from './JobsListContext';
+import jobLists from './../utils/data.json';
 
 interface Props {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
-
 const JobListProvider = ({ children }: Props) => {
-    const [filtersArray, setFiltersArray] = useState<string[]>([])
+    const [filtersArray, setFiltersArray] = useState<string[]>([]);
 
-    const displayFilteredJobs = () =>{
-       let filteredData = jobLists.filter((jobItem)=> filtersArray.find((filter)=> jobItem.tools.includes(filter) || jobItem.languages.includes(filter)))
-       return filteredData.length > 0 ? filteredData : jobLists
-    }
+    const displayFilteredJobs = () => {
+        let filteredData = jobLists.filter((jobItem) =>
+            filtersArray.find((filter) => jobItem.tools.includes(filter) || jobItem.languages.includes(filter)),
+        );
+        return filteredData.length > 0 ? filteredData : jobLists;
+    };
 
-  return (
-    <JobListContext.Provider value={{jobLists, filtersArray, setFiltersArray, displayFilteredJobs}}>{children}</JobListContext.Provider>
-  );
+    return (
+        <JobListContext.Provider value={{ jobLists, filtersArray, setFiltersArray, displayFilteredJobs }}>
+            {children}
+        </JobListContext.Provider>
+    );
 };
 
 export default JobListProvider;
