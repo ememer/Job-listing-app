9ad528d25cd029498223fabd04cdaf9048cb0348
ememer <46853050+ememer@users.noreply.github.com>
2022-12-10 17:57:16 +0100

    Created Secound Step form logic

--- src/components/EmployerFourthStepForm.tsx
+++ src/components/EmployerFourthStepForm.tsx
@@ -1,27 +1,79 @@
-import React, { useContext } from 'react';
+import React, { ChangeEvent, useContext } from 'react';
 import { FormContextProvider } from '../@types/FormContext';
+import { JobListObject } from '../@types/JobListTypes';
 import { FormContext } from '../Context/FormContext';
 
 const EmployerFourthStepForm = () => {
     const { employerAnnouncement, setEmployerAnnouncement } = useContext(FormContext) as FormContextProvider;
 
+    console.log(employerAnnouncement);
+
     return (
         <div className="employer__fields">
             <div>
                 <label>Title</label>
-                <input type="text" title="Insert announcement title"></input>
+                <input
+                    value={employerAnnouncement.description.title}
+                    onChange={(e: ChangeEvent) =>
+                        setEmployerAnnouncement((pS: JobListObject) => ({
+                            ...pS,
+                            description: {
+                                ...pS.description,
+                                title: (e.target as HTMLInputElement).value,
+                            },
+                        }))
+                    }
+                    type="text"
+                    title="Insert announcement title"
+                ></input>
             </div>
             <div>
                 <label>Description</label>
-                <textarea rows={10} title="Insert announcement title" />
+                <textarea
+                    value={employerAnnouncement.description.text}
+                    onChange={(e: ChangeEvent) =>
+                        setEmployerAnnouncement((pS: JobListObject) => ({
+                            ...pS,
+                            description: {
+                                ...pS.description,
+                                text: (e.target as HTMLInputElement).value,
+                            },
+                        }))
+                    }
+                    rows={10}
+                    title="Insert announcement title"
+                />
             </div>
             <div>
                 <label>Additional subtitle</label>
-                <input type="text" title="Insert additional subtitle"></input>
+                <input
+                    value={employerAnnouncement.description.subtitle}
+                    onChange={(e: ChangeEvent) =>
+                        setEmployerAnnouncement((pS: JobListObject) => ({
+                            ...pS,
+                            description: {
+                                ...pS.description,
+                                subtitle: (e.target as HTMLInputElement).value,
+                            },
+                        }))
+                    }
+                    type="text"
+                    title="Insert additional subtitle"
+                ></input>
             </div>
             <div>
                 <label>Additional description</label>
                 <textarea
+                    value={employerAnnouncement.description.subtext}
+                    onChange={(e: ChangeEvent) =>
+                        setEmployerAnnouncement((pS: JobListObject) => ({
+                            ...pS,
+                            description: {
+                                ...pS.description,
+                                subtext: (e.target as HTMLInputElement).value,
+                            },
+                        }))
+                    }
                     rows={5}
                     title="Insert
                          additional description"
--- src/components/EmployerSecondStepForm.tsx
+++ src/components/EmployerSecondStepForm.tsx
@@ -1,9 +1,59 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { FormContextProvider } from '../@types/FormContext';
+import { JobListObject } from '../@types/JobListTypes';
 import { FormContext } from './../Context/FormContext';
 
 const EmployerSecondStepForm = () => {
-    const { employerAnnouncement, setAnnouncementField } = useContext(FormContext) as FormContextProvider;
+    const [userInputLangs, setUserInputLangs] = useState('');
+    const [userInputTools, setUserInputTools] = useState('');
+
+    const { employerAnnouncement, setEmployerAnnouncement, setAnnouncementField } = useContext(
+        FormContext,
+    ) as FormContextProvider;
+
+    const protectWhiteSpaces = (text: string): string => {
+        let re = /  +/g;
+        return text.replaceAll(re, ' ');
+    };
+
+    const convertStringToArray = (text: string): string[] => {
+        let commaIndexes: number[] = [];
+        let index = 0;
+        for (const character of text) {
+            if (character === ',') {
+                commaIndexes.push(index);
+            }
+            index++;
+        }
+
+        let arrOfWords: string[] = [];
+        let arrayIndex: number = 1;
+
+        arrOfWords.push(text.slice(0, commaIndexes[0]));
+        commaIndexes.forEach((num) => {
+            arrOfWords.push(text.slice(num + 2, commaIndexes[arrayIndex]));
+            arrayIndex++;
+        });
+
+        if (!arrOfWords.includes('')) {
+            return arrOfWords;
+        }
+
+        return arrOfWords.filter((elem) => elem !== '');
+    };
+
+    useEffect(() => {
+        setEmployerAnnouncement((pS: JobListObject) => ({
+            ...pS,
+            tools: convertStringToArray(userInputTools),
+        }));
+
+        setEmployerAnnouncement((pS: JobListObject) => ({
+            ...pS,
+            languages: convertStringToArray(userInputLangs),
+        }));
+    }, [setEmployerAnnouncement, userInputTools, userInputLangs]);
+
     return (
         <div className="employer__fields">
             <div>
@@ -26,11 +76,50 @@ const EmployerSecondStepForm = () => {
             </div>
             <div>
                 <label>Technical languages</label>
-                <input type="text" title="Insert technical languages"></input>
+                <input
+                    id="LANGS"
+                    value={userInputLangs}
+                    onChange={(e) => {
+                        setUserInputLangs(protectWhiteSpaces(e.target.value));
+                    }}
+                    placeholder="Separate words with a comma e.g. HTML, CSS,"
+                    type="text"
+                    title="Insert technical languages"
+                ></input>
+                {employerAnnouncement.languages.length > 0 && (
+                    <div>
+                        <span>Click on each of element to remove</span>
+                        <ul>
+                            {employerAnnouncement.languages.map((lang, idx) => (
+                                <li key={`${idx}#${lang}`}>{lang}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
             </div>
             <div>
                 <label>Technical Tools</label>
-                <input type="text" title="Insert technical tools"></input>
+                <input
+                    id="TOOLS"
+                    value={userInputTools}
+                    onChange={(e) => {
+                        setUserInputTools(protectWhiteSpaces(e.target.value));
+                    }}
+                    placeholder="Separate words with a comma e.g React, Sass,"
+                    type="text"
+                    title="Insert technical tools"
+                ></input>
+
+                {employerAnnouncement.tools.length > 0 && (
+                    <div>
+                        <span>Click on each of element to remove</span>
+                        <ul>
+                            {employerAnnouncement.tools.map((lang, idx) => (
+                                <li key={`${idx}#${lang}`}>{lang}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
             </div>
         </div>
     );
